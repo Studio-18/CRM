8b91977a7483a420591da7a3e7d6de012477925b
Abe Menashy <abestbet1@gmail.com>
2016-02-17 11:45:54 -0800

    saving edited canvas to cloudinary

--- www/views/uploadImage/upload.js
+++ www/views/uploadImage/upload.js
@@ -1,5 +1,38 @@
 angular.module('App')
 .controller('uploadController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils, Upload) {
+        var x = "black"
+
+        $scope.color =  function(obj) {
+          console.log('working')
+          
+    switch (obj) {
+        case "green":
+            x = "green";
+            console.log(x)
+            break;
+        case "blue":
+            x = "blue";
+            break;
+        case "red":
+            x = "red";
+            break;
+        case "yellow":
+            x = "yellow";
+            break;
+        case "orange":
+            x = "orange";
+            break;
+        case "black":
+            x = "black";
+            break;
+        case "white":
+            x = "white";
+            break;
+    }
+    if (x == "white") y = 14;
+    else y = 2;
+
+}
 
         $scope.editFiles = function() {
             var canvas = document.getElementById("myCanvas");
@@ -9,64 +42,124 @@ angular.module('App')
             ctx.lineWith = 2;
             ctx.drawImage(img, 10, 10);
 
-            
-           canvas.addEventListener('click', function(e){
-            console.log('hello')
-   var mouseX = e.pageX - this.offsetLeft;
-   var mouseY = e.pageY - this.offsetTop;
-  
-   paint = true;
-   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-   console.log(this.offsetTop)
-   redraw();
-})
+            var flag = false
+            var prevX = 0
+            var currX = 0
+            var prevY = 0
+            var currY = 0
+            var dot_flag = false
+            // var x = "black"
+            var y = 2
 
-  canvas.addEventListener('mousemove', function(e){
-    if(paint){
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-      redraw();
-    }
-  });
+            w = canvas.width;
+            h = canvas.height;
 
- canvas.addEventListener('mouseup', function(e){
-   paint = false;
- });
+            canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
 
- canvas.addEventListener('mouseleave', function(e){
-   paint = false;
- });
+//     $scope.color =  function(obj) {
+//     switch (obj.id) {
+//         case "green":
+//             x = "green";
+//             break;
+//         case "blue":
+//             x = "blue";
+//             break;
+//         case "red":
+//             x = "red";
+//             break;
+//         case "yellow":
+//             x = "yellow";
+//             break;
+//         case "orange":
+//             x = "orange";
+//             break;
+//         case "black":
+//             x = "black";
+//             break;
+//         case "white":
+//             x = "white";
+//             break;
+//     }
+//     if (x == "white") y = 14;
+//     else y = 2;
 
- var clickX = new Array();
-var clickY = new Array();
-var clickDrag = new Array();
-var paint;
+// }
 
-function addClick(x, y, dragging)
-{
-  clickX.push(x);
-  clickY.push(y);
-  clickDrag.push(dragging);
-}
 
-function redraw(){
-  // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
-  
-  ctx.strokeStyle = "#df4b26";
-  ctx.lineJoin = "round";
-  ctx.lineWidth = 5;
-      
-  for(var i=0; i < clickX.length; i++) {    
+$scope.draw = function() {
     ctx.beginPath();
-    if(clickDrag[i] && i){
-      ctx.moveTo(clickX[i-1], clickY[i-1]);
-     }else{
-       ctx.moveTo(clickX[i]-1, clickY[i]);
-     }
-     ctx.lineTo(clickX[i], clickY[i]);
-     ctx.closePath();
-     ctx.stroke();
-  }
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
 }
+
+$scope.erase = function() {
+    // var m = confirm("Want to clear");
+    // if (m) {
+        ctx.clearRect(0, 0, w, h);
+
+        var img = document.getElementById("testImage");
+            
+            ctx.drawImage(img, 10, 10);
+        // document.getElementById("canvasimg").style.display = "none";
+    // }
+}
+
+$scope.save = function() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+    $scope.uploadFiles(dataURL)
+}
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+    }
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            $scope.draw();
+        }
+    }
+}
+
+
+
 
         }
 
@@ -76,9 +169,9 @@ function redraw(){
       
         
           picFiles.upload = Upload.upload({
-            url: "https://api.cloudinary.com/v1_1/" + "**********" + "/upload",
+            url: "https://api.cloudinary.com/v1_1/" + "dsduoklii" + "/upload",
             data: {
-              upload_preset: "********",
+              upload_preset: "cum5djmt",
               tags: 'myphotoalbum',
               context: 'photo=' + $scope.title,
               file: picFiles
@@ -99,9 +192,11 @@ function redraw(){
         'images' : ticket.image
         
 
-      }).then(function(response ){
-        // call function to refresh page
-      } )
+      })
+
+      //         .then(function(response ){
+      //   // call function to refresh page
+      // } )
               
   
 
